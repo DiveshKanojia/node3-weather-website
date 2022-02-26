c7de51656e83804f6b74b1065c36d0cdbe1d796b
Divesh Kanojia <divesh.kanojia@techchefz.com>
2022-02-26 17:56:05 +0530

    Some more data is add to forecast

--- src/uitls/forecast.js
+++ src/uitls/forecast.js
@@ -14,7 +14,7 @@ const forecast = (lat, long, callback) => {
             callback('Unable to find location', undefined)
         } else {
             console.log(body);
-            callback(undefined, `${body.current["weather_descriptions"][0]}. It is currently ${body.current["temperature"]} Degree out. It feels like ${body.current["feelslike"]} Degree out. Wind seep will be ${body.current["wind_speed"]} and the humidity will be ${body.current["humidity"]}`)
+            callback(undefined, `${body.current["weather_descriptions"][0]}. It is currently ${body.current["temperature"]} Degree out. It feels like ${body.current["feelslike"]} Degree out. Wind seep is ${body.current["wind_speed"]} and the humidity is ${body.current["humidity"]}`)
         }
     })
 }
